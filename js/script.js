6cf7800667f84079898bcbd8d67bb205b29478f0
iara <isegada@encodesa.com.ar>
2024-10-10 12:54:17 -0300

    seccion testimonials con swiper

--- js/script.js
+++ js/script.js
@@ -22,3 +22,66 @@ sr.reveal('.cont-testimonials', {delay:450});
 
 sr.reveal('.text-final', {delay:450});
 sr.reveal('.img-final', {delay:450});
+
+
+
+// TESTIMONIALS OWL CAROUSEL 
+
+// $('.owl-carousel').owlCarousel({
+//     center: true,
+//     items:3,
+//     loop:true,
+//     margin:10,
+//     responsive:{
+//         600:{
+//             items:3
+//         }
+//     }
+// });
+
+// $('.owl-carousel').owlCarousel({
+//     center: true,
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+
+
+const swiper = new Swiper('.testimonials-slider', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+    },
+    autoplay: {
+        delay: 5000,
+      },
+  });
